b5f4ef380bdc235c1a3ca1620d22fbb1f5b1c687
nedaras <nedas.pranskunas@gmail.com>
2024-03-19 18:34:34 +0200

    We're reading entries, but WADFile struct is fucking big.

--- src/main.zig
+++ src/main.zig
@@ -7,17 +7,16 @@ pub fn main() !void {
     var wad_file = try wad.openFile("Aatrox.wad.client");
     defer wad_file.close();
 
-    print("COOL\n", .{});
+    while (try wad_file.next()) |entry| {
+        print("hash: {}, size: {}, offset: {}\n", .{ entry.hash, entry.size, entry.offset });
+    }
+
+    print("sizeof WADFile: {}\n", .{@sizeOf(wad.WADFile)}); // damm this shi is large, bad we need a ptr to a buffer
+    print("COOL {}\n", .{wad_file.header.entries_count});
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const allocator = gpa.allocator();
     _ = allocator;
-
-    var file = try std.fs.cwd().openFile("Aatrox.wad.client", .{});
-    defer file.close();
-
-    var buffer_reader = std.io.bufferedReader(file.reader());
-    _ = buffer_reader;
 }
