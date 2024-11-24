2b78a7e75ddaeb9b64739ec5c26c856f6c49bd4b
nedaras <nedas.pranskunas@gmail.com>
2024-11-24 13:17:04 +0200

    Decompressing wad file.

--- src/main.zig
+++ src/main.zig
@@ -125,7 +125,7 @@ pub fn main_buildhashes() !void {
     var hashes = Hashes.init(allocator);
     defer hashes.deinit();
 
-    while (true) {
+    while (true) { // zig implemintation is rly rly slow
         if (mem.indexOfScalar(u8, buf[start..end], '\n')) |pos| {
             try writer.writeAll(buf[start .. start + pos]);
             start += pos + 1;
@@ -164,7 +164,7 @@ pub fn main_buildhashes() !void {
     try out_file.writeAll(final);
 }
 
-pub fn main() !void {
+pub fn main() !void { // not as fast as i wanted it to be, could async io make sence here?
     var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
     defer _ = gpa.deinit();
 
@@ -175,7 +175,11 @@ pub fn main() !void {
 
     _ = args.next().?;
 
-    const src = args.next() orelse return error.ArgumentSrcFileMissing; // now try to extract only a file
+    const src = args.next() orelse return error.ArgumentSrcFileMissing;
+    const dst = args.next() orelse return error.ArgumentDstDirMissing;
+
+    var out_dir = try fs.cwd().openDir(dst, .{});
+    defer out_dir.close();
 
     const hashes_file = try fs.cwd().openFile(".hashes", .{});
     defer hashes_file.close();
@@ -218,122 +222,71 @@ pub fn main() !void {
         assert(4 * gb > entry.decompressed_len);
         assert(4 * gb > entry.offset);
 
-        if (getPath(hashes, entry.hash)) |path| {
-            std.debug.print("{s}\n", .{path});
-        }
-
-        switch (entry.entry_type) {
+        if (getPath(hashes, entry.hash)) |path| switch (entry.entry_type) {
             .raw => {
-                //const pos = try file_stream.getPos();
-                //try file_stream.seekTo(entry.offset);
+                const pos = file_stream.pos;
 
-                //assert(entry.compressed_len == entry.decompressed_len);
-                //assert(file_stream.buffer[file_stream.pos..].len >= entry.compressed_len);
+                file_stream.pos = @intCast(entry.offset);
+                defer file_stream.pos = pos;
 
-                //const in = file_stream.buffer[file_stream.pos .. file_stream.pos + entry.compressed_len];
+                assert(file_mapping.view.len - file_stream.pos >= entry.compressed_len); // add these of checks to hashes file
 
-                //const checksum = xxhash.XxHash3(64).hash(in);
-                //assert(checksum == entry.checksum);
+                const out = file_stream.buffer[file_stream.pos .. file_stream.pos + entry.decompressed_len];
 
-                //try file_stream.seekTo(pos);
-            },
-            .zstd, .gzip, .zstd_multi => {
-                //const pos = try file_stream.getPos();
-                //try file_stream.seekTo(entry.offset);
-
-                //assert(file_stream.buffer[file_stream.pos..].len >= entry.compressed_len);
-
-                //const in = file_stream.buffer[file_stream.pos .. file_stream.pos + entry.compressed_len];
-
-                //const magic = [_]u8{ 0x28, 0xB5, 0x2f, 0xfd };
-                //assert(mem.eql(u8, in[0..4], &magic));
-
-                //const checksum = xxhash.XxHash3(64).hash(in);
-                //assert(checksum == entry.checksum);
-
-                //try file_stream.seekTo(pos);
-            },
-            .link => |t| { // hiping that gzip in zig is now slow.
-                std.debug.print("warn: idk how to handle, {s}.\n", .{@tagName(t)});
-            },
-        }
-    }
-}
-
-pub fn parsing_main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
-    defer _ = gpa.deinit();
-
-    const allocator = gpa.allocator(); // todo: use  c_allocator on unsafe release modes
-
-    var args = try std.process.argsWithAllocator(allocator);
-    defer args.deinit();
-
-    _ = args.next().?;
-
-    const src = args.next() orelse return error.ArgumentSrcFileMissing; // now try to extract only a file
-    const dst = args.next() orelse return error.ArgumentDstDirMissing;
-
-    comptime assert(@sizeOf(Header) == 272);
-    comptime assert(@sizeOf(Entry) == 32);
-
-    const file = try fs.cwd().openFile(src, .{});
-    defer file.close();
-
-    const file_len = try file.getEndPos();
-    const maping = try windows.CreateFileMappingA(file.handle, null, windows.PAGE_READONLY, 0, 0, null);
-    defer windows.CloseHandle(maping);
-
-    const file_buf = try windows.MapViewOfFile(maping, windows.FILE_MAP_READ, 0, 0, 0);
-    defer windows.UnmapViewOfFile(file_buf);
-
-    var file_stream = io.fixedBufferStream(file_buf[0..file_len]);
-    const reader = file_stream.reader();
-
-    var out_dir = try fs.cwd().openDir(dst, .{});
-    defer out_dir.close();
-
-    const header = try reader.readStruct(Header); // idk if league uses a specific endian, my guess is that they do not
-
-    assert(mem.eql(u8, &header.magic, "RW"));
-    assert(header.version.major == 3);
-    assert(header.version.minor == 3);
-
-    var out_list = std.ArrayList(u8).init(allocator);
-    defer out_list.deinit();
-
-    var scrape_buf: [256]u8 = undefined;
-    for (header.entries_len) |_| {
-        const entry = try reader.readStruct(Entry);
-        switch (entry.entry_type) {
-            .zstd => { // performance not bad, but we probably could multithread (it would be pain to implement)
-                const pos = try file_stream.getPos();
-                try file_stream.seekTo(entry.offset);
-
-                try out_list.ensureTotalCapacity(entry.decompressed_len);
-
-                assert(out_list.capacity >= entry.decompressed_len);
-                assert(file_len - file_stream.pos >= entry.compressed_len);
-
-                const in = file_stream.buffer[file_stream.pos .. file_stream.pos + entry.compressed_len];
-                const out = out_list.allocatedSlice()[0..entry.decompressed_len];
-
-                const zstd_len = c.ZSTD_decompress(out.ptr, out.len, in.ptr, in.len); // we could have stack buf and just fill it and write to file, and thus we would not need to alloc mem.
-                if (c.ZSTD_isError(zstd_len) == 1) {
-                    std.debug.print("err: {s}\n", .{c.ZSTD_getErrorName(zstd_len)});
+                if (fs.path.dirname(path)) |dir| {
+                    try out_dir.makePath(dir);
                 }
 
-                try file_stream.seekTo(pos);
-
-                const name = try std.fmt.bufPrint(&scrape_buf, "{x}.dds", .{entry.hash});
-                const out_file = try out_dir.createFile(name, .{});
+                const out_file = out_dir.createFile(path, .{}) catch |err| switch (err) {
+                    error.BadPathName => {
+                        std.debug.print("warn: invalid path:  {s}.\n", .{path});
+                        continue;
+                    },
+                    else => return err,
+                };
                 defer out_file.close();
 
                 try out_file.writeAll(out);
             },
-            .raw, .gzip, .link, .zstd_multi => |t| { // hiping that gzip in zig is now slow.
-                std.debug.print("warn: idk how to handle, {s}.\n", .{@tagName(t)});
+            .zstd, .zstd_multi => {
+                const pos = file_stream.pos;
+
+                file_stream.pos = @intCast(entry.offset);
+                defer file_stream.pos = pos;
+
+                try out_list.ensureTotalCapacity(entry.decompressed_len);
+                out_list.items.len = entry.decompressed_len;
+
+                assert(file_mapping.view.len - file_stream.pos >= entry.compressed_len); // add these of checks to hashes file
+
+                const in = file_stream.buffer[file_stream.pos .. file_stream.pos + entry.compressed_len];
+                const out = out_list.items;
+
+                const zstd_len = c.ZSTD_decompress(out.ptr, out.len, in.ptr, in.len); // we could have stack buf and just fill it and write to file, and thus we would not need to alloc mem.
+                if (c.ZSTD_isError(zstd_len) == windows.TRUE) {
+                    std.debug.print("err: {s}\n", .{c.ZSTD_getErrorName(zstd_len)});
+                }
+
+                if (fs.path.dirname(path)) |dir| {
+                    try out_dir.makePath(dir);
+                }
+
+                const out_file = out_dir.createFile(path, .{}) catch |err| switch (err) {
+                    error.BadPathName => {
+                        std.debug.print("warn: invalid path:  {s}.\n", .{path});
+                        continue;
+                    },
+                    else => return err,
+                };
+                defer out_file.close();
+
+                try out_file.writeAll(out);
             },
+            .link, .gzip => |t| { // hiping that gzip in zig is now slow.
+                std.debug.print("warn: idk how to handle {s}, path: {s}.\n", .{ @tagName(t), path });
+            },
+        } else {
+            std.debug.print("unknown file: 0x{X}\n", .{entry.hash});
         }
     }
 }
